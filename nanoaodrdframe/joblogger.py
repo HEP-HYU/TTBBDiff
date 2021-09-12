c5c529665486fd5d79f07ad530bbd833c5e00abd
Tae Jeong Kim <monttj@gmail.com>
2021-09-12 23:04:58 +0900

    add merge

--- nanoaodrdframe/joblogger.py
+++ nanoaodrdframe/joblogger.py
@@ -21,21 +21,35 @@ def print_numfile( dirpath, dosum=False):
   if dosum:
     print ( dataset[7] + "/" + dataset[6] + " --> " + str(sum) + " files")
 
+
+def merge( dirpath ):
+  print dirpath
+  for (root,dirs,files) in os.walk( dirpath):
+    if files:
+      dataset = root.split("/")
+      pardir = root[:-len(dataset[-1])] 
+      fnames = ' '
+      for filename in files:
+        fnames = fnames + ' ' + root+"/"+filename
+      cmd = 'hadd -f ' + pardir+"/"+dataset[-1] +'.root' + fnames
+      #print cmd
+      os.system(cmd)
+
 if __name__ == '__main__':
 
     from optparse import OptionParser
 
     parser = OptionParser()
     parser.usage = """
-    %prog [options] <sample_dir>
-    
-    Print the numnber of processed files.
+    %prog -r <output_dir> <sample_list>
+    %prog -s <output_dir> <sample_list>
+    %prog -m <output_dir>
     """
 
     parser.add_option("-r", "--report", dest="report",
                       action = 'store_true',
                       default=False,
-                      help='Print report for bad jobs.')
+                      help='Print number of files')
 
     parser.add_option("-s", "--submit", dest="submit",
                       action = 'store_true',
@@ -51,15 +65,20 @@ if __name__ == '__main__':
     (options,args) = parser.parse_args()
 
     fpath = args[0]
-    filelist = args[1]
-    f = open(filelist,'r')
-    lines = f.read().splitlines()
 
     if options.report:
+        filelist = args[1]
+        f = open(filelist,'r')
+        lines = f.read().splitlines()
+
         print_numfile( fpath )
         for line in lines :
           print_numfile( line, True  )
     if options.submit:
+        filelist = args[1]
+        f = open(filelist,'r')
+        lines = f.read().splitlines() 
+
         for line in lines :
           dirnames = line.split("/")
           yearname = dirnames[6]
@@ -73,8 +92,9 @@ if __name__ == '__main__':
 
           logfilename = dirnames[7] + '_' + dirnames[6] + '.txt'
           cmd = './skimnanoaod.py -F --split 25 -Y ' + year + ' ' + line + ' ' + fpath + '/' + dirnames[7] +  '/' + dirnames[6] + ' > ' + logfilename 
-          #print cmd
-          os.system(cmd)
+          print cmd
+          #os.system(cmd)
     if options.merge:
         print ("merge")
+        merge( fpath ) 
 
